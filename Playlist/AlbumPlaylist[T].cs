4aca616c2aceecfedcf9cbd452bf20639e299908
Stefan Micheelsen <stufkan@gmail.com>
2013-04-13 15:26:24 +0200

    Albumplaylist and Artistplaylist added

---
+++ Playlist/AlbumPlaylist[T].cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeadDog.Audio.Libraries;
+
+namespace DeadDog.Audio
+{
+    class AlbumPlaylist<T> : IPlaylist<T>
+    {
+        List<PlaylistEntry<T>> entries;
+        int index;
+
+        public PlaylistEntry<T> CurrentEntry
+        {
+            get
+            {
+                return index < 0 ? null : entries[index];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (index == -2)
+                return false;
+
+            index++;
+            if (index < entries.Count)
+                return true;
+            else
+            {
+                index = -2;
+                return false;
+            }
+        }
+
+        public bool MovePrevious()
+        {
+            if (index == -2)
+                return false;
+
+            index--;
+            if (index < 0)
+                return true;
+            else
+            {
+                index = -2;
+                return false;
+            }
+
+        }
+
+        public bool MoveRandom()
+        {
+            if (entries.Count > 0)
+            {
+                Random r = new Random();
+                index = r.Next(entries.Count);
+                currentEntry = entries[index];
+                return true;
+            }
+            else return false;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+            currentEntry = null;
+        }
+
+
+    }
+}
---
+++ Playlist/ArtistPlaylist[T].cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeadDog.Audio.Libraries;
+
+namespace DeadDog.Audio
+{
+    class ArtistPlaylist<T> : IPlaylist<T>
+    {
+        List<AlbumPlaylist<T>> albums;
+        int index;
+        Artist artist;
+
+        public ArtistPlaylist(Artist artist)
+        {
+            this.artist = artist;
+            this.artist.Albums.a
+        }
+
+        public PlaylistEntry<T> CurrentEntry
+        {
+            get { return albums[index].CurrentEntry; }
+        }
+
+        public bool MoveNext()
+        {
+            if (index == -2)
+            {
+                return false;
+            }
+            else if (index == -1)
+            {
+                index++;
+                while (albums[index].MoveNext() == false)
+                {
+                    index++;
+                    if (index >= albums.Count)
+                    {
+                        index = -2;
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else if (index < albums.Count)
+            {
+                if (albums[index].MoveNext())
+                    return true;
+                else
+                {
+                    index++;
+                    albums[index].MoveNext();
+                    return true;
+                }
+            }
+            else return false;
+        }
+
+        public bool MovePrevious()
+        {
+            if (index == -2)
+                return false;
+            else if(index == -1)
+        }
+
+        public bool MoveRandom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
