7dc37f10789edfc1aaea9afe69f5bf0ca4fa3c12
deaddog <mikkelslsl@gmail.com>
2013-04-13 15:45:22 +0200

    Implemented add and remove events for tracks.

---
+++ Libraries/Events/TrackEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeadDog.Audio.Libraries
+{
+    /// <summary>
+    /// Represents a method that handles the <see cref="DeadDog.Audio.Libraries.Track.TrackCollection.TrackAdded"/> and the <see cref="DeadDog.Audio.Libraries.Track.TrackCollection.TrackRemoved"/> events.
+    /// </summary>
+    /// <param name="collection">The track collection.</param>
+    /// <param name="e">The <see cref="TrackEventArgs"/> instance containing the event data.</param>
+    public delegate void TrackEventHandler(Track.TrackCollection collection, TrackEventArgs e);
+}
