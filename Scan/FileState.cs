c9595ad9d302ee9a14a5f48e17e0905123fc7ed3
deaddog <mikkelslsl@gmail.com>
2013-09-30 20:24:31 +0200

    ScanFileEvent added.

---
+++ Scan/FileState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeadDog.Audio.Scan
+{
+    public enum FileState
+    {
+        Added = 0x01,
+        Updated = 0x02,
+        Error = 0x04,
+        AddError = 0x0C,
+        UpdateError = 0x14,
+        Removed = 0x20
+    }
+}
