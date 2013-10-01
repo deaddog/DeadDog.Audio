fb12b18f70b0b5b8053291dd388c37eec11115f2
deaddog <mikkelslsl@gmail.com>
2013-10-01 06:39:52 +0200

    Removed obsolete scanner-state classes.

--- Scan/ScanProgressChangedEventHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DeadDog.Audio.Scan
-{
-    public delegate void ScanProgressChangedEventHandler(object sender, ScanProgressChangedEventArgs e);
-}
