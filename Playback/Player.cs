b4ef1d2902c79bfd7d0d2f99dd22f0205eb35e1b
deaddog <mikkelslsl@gmail.com>
2013-12-26 17:49:29 +0100

    Added a Player class for handling playback of an IPlaylist using an IPlayback.

---
+++ Playback/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DeadDog.Audio.Playback
+{
+    public class Player<T> where T : DeadDog.Audio.Libraries.Track
+    {
+        private IPlaylist<T> playlist;
+        private IPlayback playback;
+    }
+}
