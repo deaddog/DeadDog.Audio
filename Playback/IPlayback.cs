a185f085d67e481fa3cad68ec206b9c14fee2208
deaddog <mikkelslsl@gmail.com>
2013-12-26 17:36:21 +0100

    Added an interface for implementing audio playback.

---
+++ Playback/IPlayback.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DeadDog.Audio.Playback
+{
+    public interface IPlayback : IDisposable
+    {
+        public void Open(string filepath);
+        public bool Close();
+
+        public bool Play();
+        public bool Pause();
+        public bool Stop();
+
+        public bool Seek(PlayerSeekOrigin origin, uint offset);
+
+        public PlayerStatus Status { get; }
+        public uint Position { get; set; }
+        public uint Length { get; }
+    }
+}
