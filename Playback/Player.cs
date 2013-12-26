d093d77923aea9e27993e0e73c60d4bec7abf547
deaddog <mikkelslsl@gmail.com>
2013-12-26 18:01:21 +0100

    Implemented state properties for the Player class.

--- Playback/Player.cs
+++ Playback/Player.cs
@@ -2,9 +2,47 @@
 
 namespace DeadDog.Audio.Playback
 {
-    public class Player<T> where T : DeadDog.Audio.Libraries.Track
+    public class Player<T> : IDisposable
+        where T : DeadDog.Audio.Libraries.Track
     {
         private IPlaylist<T> playlist;
         private IPlayback playback;
+
+        public Player(IPlaylist<T> playlist, IPlayback playback)
+        {
+            this.playlist = playlist;
+            this.playback = playback;
+        }
+
+        public PlayerStatus Status
+        {
+            get { return playback.Status; }
+        }
+        public uint Position
+        {
+            get { return playback.Position; }
+            set { Seek(PlayerSeekOrigin.Begin, value); }
+        }
+        public uint Length
+        {
+            get { return playback.Length; }
+        }
+        public double PercentPlayed
+        {
+            get { return (double)Position / (double)Length; }
+        }
+
+        void IDisposable.Dispose()
+        {
+            Stop();
+            playback.Close();
+            playback.Dispose();
+            playback = null;
+            this.Dispose();
+        }
+
+        protected void Dispose()
+        {
+        }
     }
 }
