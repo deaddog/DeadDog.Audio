0b936d881c722563ffc6596bf05f6b97bab7d5d8
deaddog <mikkelslsl@gmail.com>
2013-12-30 12:39:09 +0100

    Merged QueueEntry[T].cs and QueueEntry[T, Q].cs into QueueEntry.cs.

--- Playlist/QueueEntry.cs
+++ Playlist/QueueEntry.cs
@@ -4,6 +4,30 @@ using System.Text;
 
 namespace DeadDog.Audio
 {
+    public class QueueEntry<T>
+    {
+        private static int nextid = 0;
+        private int id;
+        private PlaylistEntry<T> entry;
+
+        public PlaylistEntry<T> Entry
+        {
+            get { return entry; }
+        }
+
+        public QueueEntry(PlaylistEntry<T> entry)
+        {
+            this.entry = entry;
+            this.id = nextid;
+            nextid++;
+        }
+
+        public int CompareTo(QueueEntry<T> x)
+        {
+            return this.id.CompareTo(x.id);
+        }
+    }
+
     public abstract class QueueEntry<T, Q>
     {
         private static int nextid = 0;
@@ -36,4 +60,5 @@ namespace DeadDog.Audio
             return this.id.CompareTo(x.id);
         }
     }
+
 }
