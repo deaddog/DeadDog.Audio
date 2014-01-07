a46a65ad2b1cd390ffca600903f0ed30478b15ac
deaddog <mikkelslsl@gmail.com>
2014-01-07 22:10:14 +0100

    Added an abstract implementation of IPlaylist, for simplifying Move-operations.

---
+++ Playlist/Playlist.cs
@@ -0,0 +1,22 @@
+﻿namespace DeadDog.Audio
+{
+    public abstract class Playlist<T> : IPlaylist<T>
+    {
+        public T Entry
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public event EntryChangedEventHandler<T> EntryChanged;
+
+        public bool MoveNext()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
