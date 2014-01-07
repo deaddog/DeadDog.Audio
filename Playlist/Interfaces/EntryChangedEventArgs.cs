46569c1940cb2aa2471ba1201d463e2fd1e7cbb3
deaddog <mikkelslsl@gmail.com>
2014-01-07 17:39:07 +0100

    EntryChangedEventArgs provides a RejectChange for attempting to reject a change in a playlists Entry property. Method success is returned by the method and can be read via the Rejected property.

--- Playlist/Interfaces/EntryChangedEventArgs.cs
+++ Playlist/Interfaces/EntryChangedEventArgs.cs
@@ -7,26 +7,42 @@ namespace DeadDog.Audio
     /// </summary>
     public class EntryChangedEventArgs : EventArgs
     {
-        private bool changeAccepted;
+        private bool rejected;
+        private bool canReject;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="EntryChangedEventArgs"/> class.
         /// </summary>
-        public EntryChangedEventArgs()
+        public EntryChangedEventArgs(bool canReject)
         {
-            this.changeAccepted = true;
+            this.rejected = false;
+            this.canReject = canReject;
         }
 
         /// <summary>
-        /// Gets or sets a value indicating whether the entry change is accepted; such as whether the new entry exists.
+        /// Attempts to reject the entry change.
+        /// </summary>
+        /// <returns><c>true</c> if the entry change could be rejected; otherwise, <c>false</c>.</returns>
+        public bool RejectChange()
+        {
+            if (rejected)
+                return true;
+
+            if (canReject)
+                rejected = true;
+
+            return rejected;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the entry change is to be rejected.
         /// </summary>
         /// <value>
-        ///   <c>true</c> if the entry change is accepted; otherwise, <c>false</c>.
+        ///   <c>true</c> if the entry change will be rejected; otherwise, <c>false</c>.
         /// </value>
-        public bool ChangeAccepted
+        public bool Rejected
         {
-            get { return changeAccepted; }
-            set { changeAccepted = value; }
+            get { return rejected; }
         }
     }
 }
