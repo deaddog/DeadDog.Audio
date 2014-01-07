97d9ba10ba3b996457a3d11080b66b44f7a486f6
deaddog <mikkelslsl@gmail.com>
2014-01-07 22:33:56 +0100

    Implemented method for setting entry in Playlist. This will automatically handle event calls. Reset is called via the default constructor - this should set the default value for the constructor and will not force the event to be invoked.

--- Playlist/Playlist.cs
+++ Playlist/Playlist.cs
@@ -2,13 +2,36 @@
 {
     public abstract class Playlist<T> : IPlaylist<T>
     {
+        private T entry;
+
         public T Entry
         {
-            get { throw new System.NotImplementedException(); }
+            get { return entry; }
+        }
+
+        public Playlist()
+        {
+            Reset();
         }
 
         public event System.EventHandler EntryChanged;
         public event EntryChangingEventHandler<T> EntryChanging;
+
+        protected bool trySettingEntry(T entry, bool canReject)
+        {
+            if (EntryChanging != null)
+            {
+                EntryChangingEventArgs<T> e = new EntryChangingEventArgs<T>(entry, canReject);
+                EntryChanging(this, e);
+                if (e.Rejected)
+                    return false;
+            }
+
+            this.entry = entry;
+            if (EntryChanged != null)
+                EntryChanged(this, System.EventArgs.Empty);
+            return true;
+        }
 
         public bool MoveNext()
         {
