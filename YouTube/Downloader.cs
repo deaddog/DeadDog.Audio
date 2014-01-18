68a8a58958cb847da4e7a1b1ac1be867c611bb15
deaddog <mikkelslsl@gmail.com>
2014-01-19 00:46:03 +0100

    Implemented the actual download part in youtube downloader.

--- YouTube/Downloader.cs
+++ YouTube/Downloader.cs
@@ -46,7 +46,31 @@ namespace DeadDog.Audio.YouTube
 
         private void performLoad(YouTubeID id)
         {
+            string text;
+
+            URL mp3URL = getMp3URL(id, out text);
+            URL infoURL = new URL("https://gdata.youtube.com/feeds/api/videos/" + id.ID + "?v=2");
+
+            string xml = infoURL.GetHTML(System.Text.Encoding.UTF8).Trim('\0');
+            XDocument doc = XDocument.Load(new System.IO.StringReader(xml));
+
+            if (doc != null)
+                text = doc.Root.Element(XName.Get("title", "http://www.w3.org/2005/Atom")).Value;
+
             throw new NotImplementedException();
+        }
+        private URL getMp3URL(YouTubeID id, out string text)
+        {
+            URL mp3infoURL = new URL("http://youtubeinmp3.com/fetch/?api=advanced&video=http://www.youtube.com/watch?v=" + id.ID);
+            string html = mp3infoURL.GetHTML();
+
+            int linkIndex = html.IndexOf("<br />Link: ");
+            string link = html.Substring(linkIndex + 12);
+            link = link.Trim(' ', '\t', '\n', '\r', '\0');
+
+            text = html.Substring(7, html.IndexOf("<br />") - 12).Replace('_', ' ');
+
+            return new URL(link);
         }
 
         private string getClipPath(YouTubeID id)
