1dd5b95a92c7239907de133fcf6046e518efff3a
deaddog <mikkelslsl@gmail.com>
2014-01-18 22:02:55 +0100

    Added a YouTubeID class which will handle references to YouTube clips and parsing of urls.

---
+++ YouTube/YouTubeID.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace DeadDog.Audio.YouTube
+{
+    public class YouTubeID
+    {
+        private string id;
+
+        private YouTubeID(string id)
+        {
+            this.id = id;
+        }
+
+        public string ID
+        {
+            get { return id; }
+        }
+    }
+}
