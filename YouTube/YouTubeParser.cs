ff60478ac970642e7b2994784886cd6f789999ef
deaddog <mikkelslsl@gmail.com>
2014-01-18 23:21:15 +0100

    YouTubeParser reads YouTube title from XML file.

--- YouTube/YouTubeParser.cs
+++ YouTube/YouTubeParser.cs
@@ -1,5 +1,7 @@
 ﻿using DeadDog.Audio.Parsing;
 using System;
+using System.IO;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace DeadDog.Audio.YouTube
@@ -8,24 +10,40 @@ namespace DeadDog.Audio.YouTube
     {
         private IDataParser fallbackParser;
         private string documentPath;
+        private string directory;
 
         public YouTubeParser(string directory, IDataParser parser)
         {
             this.fallbackParser = parser;
 
-            System.IO.FileInfo file = new System.IO.FileInfo(XML.DocumentPath(directory));
-            if (!file.Exists)
-                documentPath = null;
-            else
-                documentPath = file.FullName;
+            this.directory = Path.GetFullPath(directory);
+            this.documentPath = XML.DocumentPath(directory);
         }
 
         public RawTrack ParseTrack(string filepath)
         {
             if (documentPath == null)
                 return null;
+            if (!new FileInfo(documentPath).Exists)
+                return null;
+            if (!filepath.StartsWith(directory))
+                return null;
 
-            throw new NotImplementedException();
+            XDocument document = XDocument.Load(documentPath);
+            string title = getTitle(filepath, document);
+        }
+
+        private string getTitle(string filepath, XDocument document)
+        {
+            string relative = filepath.Substring(directory.Length).TrimStart('\\');
+
+            var tracks = from e in document.Element("tracks").Elements("track")
+                         let path = e.Element("path").Value
+                         where path == relative
+                         let title = e.Element("title").Value
+                         select title;
+
+            return tracks.FirstOrDefault();
         }
     }
 }
