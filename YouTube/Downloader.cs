3ecedecc6e381aa57c12fb77e50e77828b070341
deaddog <mikkelslsl@gmail.com>
2014-01-18 22:11:15 +0100

    YouTube Downloader has a field for the directory into which files should be stored.

--- YouTube/Downloader.cs
+++ YouTube/Downloader.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DeadDog.Audio.YouTube
 {
     public class Downloader
     {
+        private string directory;
+
+        public Downloader(string directory)
+        {
+            this.directory = System.IO.Path.GetFullPath(directory).TrimEnd('\\');
+        }
+
+        private string getClipPath(YouTubeID id)
+        {
+            return directory + "\\" + id.ID + ".mp3";
+        }
     }
 }
