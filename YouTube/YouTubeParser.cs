f5b62dc4af3ab03c10ba4f752a62b60e28902218
deaddog <mikkelslsl@gmail.com>
2014-01-18 20:38:14 +0100

    Added a YouTubeParser class which implements IDataParser.

---
+++ YouTube/YouTubeParser.cs
@@ -0,0 +1,13 @@
+﻿using DeadDog.Audio.Parsing;
+using System;
+
+namespace DeadDog.Audio.YouTube
+{
+    public class YouTubeParser : IDataParser
+    {
+        public RawTrack ParseTrack(string filepath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
