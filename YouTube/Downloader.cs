a8d564315afe61892ba79dc2380a1046100e58b4
deaddog <mikkelslsl@gmail.com>
2014-01-18 22:13:56 +0100

    A dictionary in the youtube downloader maintains information about clip state.

--- YouTube/Downloader.cs
+++ YouTube/Downloader.cs
@@ -6,15 +6,28 @@ namespace DeadDog.Audio.YouTube
     public class Downloader
     {
         private string directory;
+        private Dictionary<YouTubeID, State> files;
 
         public Downloader(string directory)
         {
             this.directory = System.IO.Path.GetFullPath(directory).TrimEnd('\\');
+            this.files = new Dictionary<YouTubeID, State>();
+        }
+
+        public void Load(YouTubeID id)
+        {
         }
 
         private string getClipPath(YouTubeID id)
         {
             return directory + "\\" + id.ID + ".mp3";
+        }
+
+        public enum State
+        {
+            None,
+            Loading,
+            Loaded
         }
     }
 }
