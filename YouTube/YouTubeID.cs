ee76ebcf0d3dbca62b348457feb386d37fcc68ae
deaddog <mikkelslsl@gmail.com>
2014-01-18 22:04:28 +0100

    Added parsing methods to YouTubeID.

--- YouTube/YouTubeID.cs
+++ YouTube/YouTubeID.cs
@@ -19,6 +19,43 @@ namespace DeadDog.Audio.YouTube
             this.id = id;
         }
 
+        public static YouTubeID Parse(string s)
+        {
+            var match = Regex.Match(s, @"([\?\&]v=(?<id>[^&]+))|^(?<id>([a-zA-Z0-9_\-])+)$");
+            if (!match.Success)
+                throw new ArgumentException("String could not be parsed to a YouTube identifier.", "s");
+            else
+            {
+                YouTubeID id;
+                string idStr = match.Groups["id"].Value;
+                if (!lookup.TryGetValue(idStr, out id))
+                {
+                    id = new YouTubeID(idStr);
+                    lookup.Add(idStr, id);
+                }
+                return id;
+            }
+        }
+        public static bool TryParse(string s, out YouTubeID id)
+        {
+            var match = Regex.Match(s, @"([\?\&]v=(?<id>[^&]+))|^(?<id>([a-zA-Z0-9_\-])+)$");
+            if (!match.Success)
+            {
+                id = null;
+                return false;
+            }
+            else
+            {
+                string idStr = match.Groups["id"].Value;
+                if (!lookup.TryGetValue(idStr, out id))
+                {
+                    id = new YouTubeID(idStr);
+                    lookup.Add(idStr, id);
+                }
+                return true;
+            }
+        }
+
         public string ID
         {
             get { return id; }
