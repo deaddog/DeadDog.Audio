f724fb82f640083b1b72253276011d702e0cf7f6
deaddog <mikkelslsl@gmail.com>
2014-01-18 20:44:00 +0100

    Added fields to YouTubeParser and implemented a basic constructor. Removed the ClipInfo type. This is fully represented in XML.

--- YouTube/YouTubeParser.cs
+++ YouTube/YouTubeParser.cs
@@ -1,10 +1,32 @@
 ﻿using DeadDog.Audio.Parsing;
 using System;
+using System.Xml.Linq;
 
 namespace DeadDog.Audio.YouTube
 {
     public class YouTubeParser : IDataParser
     {
+        private IDataParser fallbackParser;
+        private string documentPath;
+        private XDocument document;
+
+        public YouTubeParser(string xmlpath, IDataParser parser)
+        {
+            this.fallbackParser = parser;
+
+            this.documentPath = xmlpath;
+            System.IO.FileInfo file = new System.IO.FileInfo(xmlpath);
+            if (!file.Exists)
+                document = createNewXML();
+            else
+                document = XDocument.Load(xmlpath);
+        }
+
+        private static XDocument createNewXML()
+        {
+            return new XDocument(new XElement("tracks"));
+        }
+
         public RawTrack ParseTrack(string filepath)
         {
             throw new NotImplementedException();
