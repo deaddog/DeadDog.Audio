a6a725b1403f0d493c9fcabdf03818b382a5742a
deaddog <mikkelslsl@gmail.com>
2014-01-18 22:03:59 +0100

    Added a dictionary to YouTubeID for maintaining a collection of all ID's.

--- YouTube/YouTubeID.cs
+++ YouTube/YouTubeID.cs
@@ -7,6 +7,12 @@ namespace DeadDog.Audio.YouTube
     public class YouTubeID
     {
         private string id;
+        private static Dictionary<string, YouTubeID> lookup;
+
+        static YouTubeID()
+        {
+            lookup = new Dictionary<string, YouTubeID>();
+        }
 
         private YouTubeID(string id)
         {
