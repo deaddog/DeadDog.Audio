5f1241d6f142980ab81b1f75e53b91fe9b832457
deaddog <mikkelslsl@gmail.com>
2014-01-18 10:25:04 +0100

    ClipInfo provides a YouTubeID field and property.

--- YouTube/ClipInfo.cs
+++ YouTube/ClipInfo.cs
@@ -4,5 +4,16 @@ namespace DeadDog.Audio.YouTube
 {
     public class ClipInfo
     {
+        private string youtubeID;
+
+        public ClipInfo(string youtubeID)
+        {
+            this.youtubeID = youtubeID;
+        }
+
+        public string YouTubeID
+        {
+            get { return youtubeID; }
+        }
     }
 }
