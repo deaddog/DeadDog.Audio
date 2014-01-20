10183d7809fc8becd0b3f7906f29e1bd79d60f9e
deaddog <mikkelslsl@gmail.com>
2014-01-20 14:45:12 +0100

    ArtistPlaylist uses a PlaylistCollection instead of inheriting from it.

--- Playlist/ArtistPlaylist.cs
+++ Playlist/ArtistPlaylist.cs
@@ -6,43 +6,45 @@ using DeadDog.Audio.Libraries;
 
 namespace DeadDog.Audio
 {
-    public class ArtistPlaylist : PlaylistCollection<Track>
+    public class ArtistPlaylist : IPlaylist<Track>
     {
         private Artist artist;
         private Dictionary<Album, AlbumPlaylist> lookup;
+        private PlaylistCollection<Track> playlists;
 
         public ArtistPlaylist(Artist artist)
         {
             this.artist = artist;
-            lookup = new Dictionary<Album, AlbumPlaylist>();
+            this.lookup = new Dictionary<Album, AlbumPlaylist>();
+            this.playlists = new PlaylistCollection<Track>();
 
             foreach (var album in artist.Albums)
             {
                 AlbumPlaylist albumplaylist = new AlbumPlaylist(album);
                 lookup.Add(album, albumplaylist);
-                addPlaylist(albumplaylist);
+                playlists.Add(albumplaylist);
             }
 
             this.artist.Albums.AlbumAdded += new AlbumEventHandler(Albums_AlbumAdded);
             this.artist.Albums.AlbumRemoved += new AlbumEventHandler(Albums_AlbumRemoved);
-
         }
 
         void Albums_AlbumRemoved(Album.AlbumCollection collection, AlbumEventArgs e)
         {
             if (lookup.ContainsKey(e.Album))
             {
-                removePlaylist(lookup[e.Album]);
+                playlists.Remove(lookup[e.Album]);
                 lookup.Remove(e.Album);
             }
-            else throw new InvalidOperationException("Playlist was not in the ArtistPlaylist and could not be removed");
+            else
+                throw new InvalidOperationException("Playlist was not in the ArtistPlaylist and could not be removed");
         }
 
         void Albums_AlbumAdded(Album.AlbumCollection collection, AlbumEventArgs e)
         {
             AlbumPlaylist albumplaylist = new AlbumPlaylist(e.Album);
             lookup.Add(e.Album, albumplaylist);
-            addPlaylist(albumplaylist);
+            playlists.Add(albumplaylist);
         }
     }
 }
