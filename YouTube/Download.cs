02c348a24ae8d2bc2af3c1ca6ef70405c9afbb32
deaddog <mikkelslsl@gmail.com>
2014-01-22 12:24:55 +0100

    Moved the methods that perform the actual YouTube download from Downlaoder to Download.

--- YouTube/Download.cs
+++ YouTube/Download.cs
@@ -27,6 +27,48 @@
             internal set { title = value; }
         }
 
+        private void performLoad(YouTubeID id)
+        {
+            string text;
+
+            URL mp3URL = getMp3URL(id, out text);
+            URL infoURL = new URL("https://gdata.youtube.com/feeds/api/videos/" + id.ID + "?v=2");
+
+            string xml = infoURL.GetHTML(System.Text.Encoding.UTF8).Trim('\0');
+            XDocument doc = XDocument.Load(new System.IO.StringReader(xml));
+
+            if (doc != null)
+                text = doc.Root.Element(XName.Get("title", "http://www.w3.org/2005/Atom")).Value;
+
+            mp3URL.GetFile(Path.Combine(directory, id.ID + ".mp3"));
+
+            XElement track = new XElement("track",
+                new XAttribute("id", id.ID),
+                new XElement("path", id.ID + ".mp3"),
+                new XElement("title", text));
+
+            lock (document)
+            {
+                document.Element("tracks").Add(track);
+                document.Save(documentPath);
+            }
+            lock (files)
+                files[id] = State.Loaded;
+        }
+        private URL getMp3URL(YouTubeID id, out string text)
+        {
+            URL mp3infoURL = new URL("http://youtubeinmp3.com/fetch/?api=advanced&video=http://www.youtube.com/watch?v=" + id.ID);
+            string html = mp3infoURL.GetHTML();
+
+            int linkIndex = html.IndexOf("<br />Link: ");
+            string link = html.Substring(linkIndex + 12);
+            link = link.Trim(' ', '\t', '\n', '\r', '\0');
+
+            text = html.Substring(7, html.IndexOf("<br />") - 12).Replace('_', ' ');
+
+            return new URL(link);
+        }
+
         public enum State
         {
             None,
