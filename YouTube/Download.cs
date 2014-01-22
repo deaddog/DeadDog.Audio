63b6502a77bdc084587bf67a7321f94acfa30185
deaddog <mikkelslsl@gmail.com>
2014-01-22 11:52:48 +0100

    Added a Download class for representing a YouTube download in progress.

---
+++ YouTube/Download.cs
@@ -0,0 +1,30 @@
+﻿namespace DeadDog.Audio.YouTube
+{
+    public class Download
+    {
+        private YouTubeID id;
+        private string path;
+        private string title;
+
+        public Download(YouTubeID id, string path)
+        {
+            this.id = id;
+            this.path = path;
+            this.title = null;
+        }
+
+        public YouTubeID ID
+        {
+            get { return id; }
+        }
+        public string Path
+        {
+            get { return path; }
+        }
+        public string Title
+        {
+            get { return title; }
+            internal set { title = value; }
+        }
+    }
+}
