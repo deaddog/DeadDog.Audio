96680dff89e8c7cae32f5e81cd379dc9dc15de49
deaddog <mikkelslsl@gmail.com>
2014-09-28 21:09:31 +0200

    Added a method in PlaylistTests for asserting the state of a given playlist.

--- Testing/Playlist/PlaylistTests.cs
+++ Testing/Playlist/PlaylistTests.cs
@@ -11,5 +11,20 @@ namespace DeadDog.Audio.Tests
     [TestClass()]
     public class PlaylistTests
     {
+        private void assertState<T>(Playlist<T> playlist, T expectedEntry, int expectedIndex) where T : class
+        {
+            Assert.AreEqual(expectedEntry, playlist.Entry);
+            Assert.AreEqual(expectedIndex, playlist.Index);
+        }
+
+        private static int PreListIndex
+        {
+            get { return Playlist<string>.PreListIndex; }
+        }
+
+        private static int PostListIndex
+        {
+            get { return Playlist<string>.PostListIndex; }
+        }
     }
 }
