8bbba0c1289365e938018f5b630a5769231a4f6c
deaddog <mikkelslsl@gmail.com>
2014-09-28 20:56:23 +0200

    Created a class stub for implementing Playlist tests.

---
+++ Testing/Playlist/PlaylistTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeadDog.Audio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeadDog.Audio.Tests
+{
+    [TestClass()]
+    public class PlaylistTests
+    {
+    }
+}
