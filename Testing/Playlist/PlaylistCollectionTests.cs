5c368faeb337bdc9fab4849d013f34d5eff77e51
deaddog <mikkelslsl@gmail.com>
2014-10-01 07:04:37 +0200

    Implemented all tests for PlaylistCollection that were implemented for Playlist.

--- Testing/Playlist/PlaylistCollectionTests.cs
+++ Testing/Playlist/PlaylistCollectionTests.cs
@@ -31,6 +31,16 @@ namespace DeadDog.Audio.Tests
         {
             return getPlaylist<string>();
         }
+        private PlaylistCollection<string> getHelloCrazyWildWorldPlaylist(bool post = false)
+        {
+            var playlist = getPlaylist<string>(getPlaylist("hello", "crazy"), getPlaylist("wild", "world"));
+            if (post)
+            {
+                playlist.MoveToLast();
+                playlist.MoveNext();
+            }
+            return playlist;
+        }
 
         private void assertState<T>(PlaylistCollection<T> playlist, T expectedEntry, int expectedIndex) where T : class
         {
@@ -71,6 +81,222 @@ namespace DeadDog.Audio.Tests
         {
             PlaylistCollection<string> playlist = new PlaylistCollection<string>();
             assertState(playlist, null, EmptyListIndex);
+        }
+
+        #endregion
+
+        #region MoveNext
+
+        [TestMethod()]
+        public void MoveNextEmptyTest()
+        {
+            var playlist = getEmptyPlaylist();
+
+            Assert.AreEqual(false, playlist.MoveNext());
+            assertState(playlist, null, EmptyListIndex);
+        }
+
+        [TestMethod()]
+        public void MoveNextTwoEntriesTest()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist();
+
+            Assert.AreEqual(true, playlist.MoveNext());
+            assertState(playlist, "hello", 0);
+
+            Assert.AreEqual(true, playlist.MoveNext());
+            assertState(playlist, "crazy", 0);
+
+            Assert.AreEqual(true, playlist.MoveNext());
+            assertState(playlist, "wild", 1);
+
+            Assert.AreEqual(true, playlist.MoveNext());
+            assertState(playlist, "world", 1);
+
+            Assert.AreEqual(false, playlist.MoveNext());
+            assertState(playlist, null, PostListIndex);
+        }
+
+        #endregion
+        #region MovePrevious
+
+        [TestMethod()]
+        public void MovePreviousEmptyTest()
+        {
+            var playlist = getEmptyPlaylist();
+
+            Assert.AreEqual(false, playlist.MovePrevious());
+            assertState(playlist, null, EmptyListIndex);
+        }
+
+        [TestMethod()]
+        public void MovePreviousTwoEntriesDirectTest()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist();
+
+            Assert.AreEqual(false, playlist.MovePrevious());
+            assertState(playlist, null, PreListIndex);
+        }
+
+        [TestMethod()]
+        public void MovePreviousTwoEntriesTest()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist(true);
+
+            Assert.AreEqual(true, playlist.MovePrevious());
+            assertState(playlist, "world", 1);
+
+            Assert.AreEqual(true, playlist.MovePrevious());
+            assertState(playlist, "wild", 1);
+
+            Assert.AreEqual(true, playlist.MovePrevious());
+            assertState(playlist, "crazy", 0);
+
+            Assert.AreEqual(true, playlist.MovePrevious());
+            assertState(playlist, "hello", 0);
+
+            Assert.AreEqual(false, playlist.MovePrevious());
+            assertState(playlist, null, PreListIndex);
+        }
+
+        #endregion
+        #region MoveToLast
+
+        [TestMethod()]
+        public void MoveToLastEmptyTest()
+        {
+            var playlist = getEmptyPlaylist();
+
+            Assert.AreEqual(false, playlist.MoveToLast());
+            assertState(playlist, null, EmptyListIndex);
+        }
+
+        [TestMethod()]
+        public void MoveToLastTwoEntriesTest()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist(true);
+
+            Assert.AreEqual(true, playlist.MoveToLast());
+            assertState(playlist, "world", 1);
+        }
+
+        #endregion
+        #region MoveToFirst
+
+        [TestMethod()]
+        public void MoveToFirstEmptyTest()
+        {
+            var playlist = getEmptyPlaylist();
+
+            Assert.AreEqual(false, playlist.MoveToFirst());
+            assertState(playlist, null, EmptyListIndex);
+        }
+
+        [TestMethod()]
+        public void MoveToFirstTwoEntriesDirectTest()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist();
+
+            Assert.AreEqual(true, playlist.MoveToFirst());
+            assertState(playlist, "hello", 0);
+        }
+
+        [TestMethod()]
+        public void MoveToFirstTwoEntriesTest()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist(true);
+
+            Assert.AreEqual(true, playlist.MoveToFirst());
+            assertState(playlist, "hello", 0);
+        }
+
+        #endregion
+        #region MoveToEntry
+
+        [TestMethod()]
+        public void MoveToEntryEmptyTest()
+        {
+            var playlist = getEmptyPlaylist();
+
+            Assert.AreEqual(false, playlist.MoveToEntry("hello"));
+            assertState(playlist, null, EmptyListIndex);
+        }
+
+        [TestMethod()]
+        public void MoveToEntryTwoEntriesUnknownKeyTest1()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist();
+
+            var state = getState(playlist);
+            Assert.AreEqual(false, playlist.MoveToEntry("unknown"));
+            assertState(playlist, state);
+        }
+
+        [TestMethod()]
+        public void MoveToEntryTwoEntriesUnknownKeyTest2()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist();
+            playlist.MoveNext();
+
+            var state = getState(playlist);
+            Assert.AreEqual(false, playlist.MoveToEntry("unknown"));
+            assertState(playlist, state);
+        }
+
+        [TestMethod()]
+        public void MoveToEntryTwoEntriesUnknownKeyTest3()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist(true);
+
+            var state = getState(playlist);
+            Assert.AreEqual(false, playlist.MoveToEntry("unknown"));
+            assertState(playlist, state);
+        }
+
+        [TestMethod()]
+        public void MoveToEntryTwoEntriesWithKeyTest1()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist();
+
+            Assert.AreEqual(true, playlist.MoveToEntry("hello"));
+            assertState(playlist, "hello", 0);
+        }
+        [TestMethod()]
+        public void MoveToEntryTwoEntriesWithKeyTest2()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist();
+
+            Assert.AreEqual(true, playlist.MoveToEntry("wild"));
+            assertState(playlist, "wild", 1);
+        }
+
+        #endregion
+
+        #region Reset
+
+        [TestMethod()]
+        public void ResetEmptyTest()
+        {
+            var playlist = getEmptyPlaylist();
+
+            playlist.Reset();
+            assertState(playlist, null, EmptyListIndex);
+        }
+        [TestMethod()]
+        public void ResetTwoEntriesTest1()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist();
+
+            playlist.Reset();
+            assertState(playlist, null, PreListIndex);
+        }
+        [TestMethod()]
+        public void ResetTwoEntriesTest2()
+        {
+            var playlist = getHelloCrazyWildWorldPlaylist(true);
+
+            playlist.Reset();
+            assertState(playlist, null, PreListIndex);
         }
 
         #endregion
