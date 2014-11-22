4ed74f3c7a5dc867ec0f7b5c6fb9eea4b8ea793a
deaddog <mikkelslsl@gmail.com>
2014-11-22 15:44:33 +0100

    Created a IPlayable<T> interface, used by the Player class to handle track-iteration.

---
+++ Source/Playlist/Interfaces/IPlayable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DeadDog.Audio
+{
+    /// <summary>
+    /// Represents an iteration on a collection of objects.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IPlayable<T>
+    {
+        /// <summary>
+        /// Gets the currently selected entry in the playable.
+        /// </summary>
+        T Entry { get; }
+
+        /// <summary>
+        /// Occurs after the <see cref="Entry"/> property is changed.
+        /// </summary>
+        event EventHandler EntryChanged;
+        /// <summary>
+        /// Occurs before the <see cref="Entry"/> property is changed.
+        /// </summary>
+        event EntryChangingEventHandler<T> EntryChanging;
+
+        /// <summary>
+        /// Moves to the next item in the playable.
+        /// </summary>
+        /// <returns>true, if the move was successful; otherwise false.</returns>
+        bool MoveNext();
+    }
+}
