d7727f9aec25b2cdee3e5513d906f54a4ccd2729
deaddog <mikkelslsl@gmail.com>
2017-06-24 23:41:12 +0200

    Simplified the Library-, Artist- and AlbumPlaylist using the new playlist type.

--- Source/Playlist/AlbumPlaylist.cs
+++ Source/Playlist/AlbumPlaylist.cs
@@ -1,108 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using DeadDog.Audio.Libraries;
+﻿using DeadDog.Audio.Libraries;
 
 namespace DeadDog.Audio
 {
-    public class AlbumPlaylist : IPlaylist<Track>
+    public class AlbumPlaylist : TrackCollectionPlaylist
     {
-        private Album album;
-        private Playlist<Track> playlist;
-
-        public AlbumPlaylist(Album album)
+        public AlbumPlaylist(Album album):base(album.Tracks)
         {
-            this.album = album;
-            this.playlist = new Playlist<Track>();
-
-            this.playlist.EntryChanged += playlist_EntryChanged;
-            this.playlist.EntryChanging += playlist_EntryChanging;
-
-            foreach (var track in album.Tracks)
-                playlist.Add(track);
-
-            this.album.Tracks.TrackAdded += new TrackEventHandler(Tracks_TrackAdded);
-            this.album.Tracks.TrackRemoved += new TrackEventHandler(Tracks_TrackRemoved);
+            Album = album;
         }
 
-        private void playlist_EntryChanged(object sender, EventArgs e)
-        {
-            if (EntryChanged != null)
-                EntryChanged(this, e);
-        }
-        private void playlist_EntryChanging(IPlayable<Track> sender, EntryChangingEventArgs<Track> e)
-        {
-            if (EntryChanging != null)
-                EntryChanging(this, e);
-        }
-
-        void Tracks_TrackAdded(TrackCollection collection, TrackEventArgs e)
-        {
-            int i = collection.IndexOf(e.Track);
-            playlist.Insert(i, e.Track);
-        }
-        void Tracks_TrackRemoved(TrackCollection collection, TrackEventArgs e)
-        {
-            playlist.Remove(e.Track);
-        }
-
-        public Track Entry
-        {
-            get { return playlist.Entry; }
-        }
-
-        public event EventHandler EntryChanged;
-        public event EntryChangingEventHandler<Track> EntryChanging;
-
-        public void Reset()
-        {
-            playlist.Reset();
-        }
-
-        public bool MoveNext()
-        {
-            return playlist.MoveNext();
-        }
-        public bool MovePrevious()
-        {
-            return playlist.MovePrevious();
-        }
-
-        public bool MoveToFirst()
-        {
-            return playlist.MoveToFirst();
-        }
-        public bool MoveToLast()
-        {
-            return playlist.MoveToLast();
-        }
-
-        public bool MoveToEntry(Track entry)
-        {
-            return playlist.MoveToEntry(entry);
-        }
-        public bool Contains(Track entry)
-        {
-            return playlist.Contains(entry);
-        }
-
-        #region IEnumerable<PlaylistEntry<Track>> Members
-
-        IEnumerator<Track> IEnumerable<Track>.GetEnumerator()
-        {
-            return (playlist as IEnumerable<Track>).GetEnumerator();
-        }
-
-        #endregion
-
-        #region IEnumerable Members
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-        {
-            return (playlist as System.Collections.IEnumerable).GetEnumerator();
-        }
-
-        #endregion
+        public Album Album { get; }
     }
 }
