6c895b070e61529eb55df6c9fdc7fc445555cdcb
deaddog <mikkelslsl@gmail.com>
2017-06-24 21:37:44 +0200

    Album watches its track collection, and maintains the Album <-> Artist relation.

--- Source/Libraries/Album.cs
+++ Source/Libraries/Album.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
 
 namespace DeadDog.Audio.Libraries
 {
@@ -15,16 +17,22 @@ namespace DeadDog.Audio.Libraries
         public Artist Artist
         {
             get { return _artist; }
-            internal set
+            private set
             {
                 if (_artist != value)
                 {
+                    if (_artist != null)
+                        _artist.Albums.Remove(this);
+
                     bool hasArtistChanged = _artist == null || value == null;
                     _artist = value;
 
                     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Artist)));
                     if (hasArtistChanged)
                         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HasArtist)));
+
+                    if (value != null)
+                        value.Albums.Add(this);
                 }
             }
         }
@@ -34,9 +42,32 @@ namespace DeadDog.Audio.Libraries
         {
             IsUnknown = title == null;
             Tracks = new TrackCollection();
+            Tracks.CollectionChanged += TracksCollectionChanged;
 
             Title = title ?? string.Empty;
             _artist = null;
+        }
+
+        private void TracksCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                foreach (Track t in e.NewItems)
+                    t.PropertyChanged += TrackPropertyChanged;
+                Artist = Tracks.AllSameOrDefault(x => x.Artist)?.Artist;
+            }
+
+            if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                foreach (Track t in e.OldItems)
+                    t.PropertyChanged -= TrackPropertyChanged;
+                Artist = Tracks.AllSameOrDefault(x => x.Artist)?.Artist;
+            }
+        }
+        private void TrackPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(Track.Artist))
+                Artist = Tracks.AllSameOrDefault(x => x.Artist)?.Artist;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
---
+++ Source/LinqExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeadDog.Audio
+{
+    internal static class LinqExtension
+    {
+        public static T AllSameOrDefault<T, TValue>(this IEnumerable<T> collection, Func<T, TValue> selector)
+        {
+            var e = collection.GetEnumerator();
+
+            if (!e.MoveNext())
+                return default(T);
+
+            var value = e.Current;
+            var cmp = selector(value);
+
+            while (e.MoveNext())
+                if (!selector(e.Current).Equals(cmp))
+                    return default(T);
+
+            return value;
+        }
+    }
+}
