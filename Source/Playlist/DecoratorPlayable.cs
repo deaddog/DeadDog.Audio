443bfebed72a65edb2fd82de7113b997a5758ef0
deaddog <mikkelslsl@gmail.com>
2017-07-11 21:21:53 +0200

    Defined decorator classes for playlist and playable.

---
+++ Source/Playlist/DecoratorPlayable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DeadDog.Audio.Playlist
+{
+    public abstract class DecoratorPlayable<T> : IPlayable<T>
+    {
+        private readonly IPlayable<T> _playable;
+
+        public DecoratorPlayable(IPlayable<T> playable)
+        {
+            _playable = playable ?? throw new ArgumentNullException(nameof(playable));
+
+            _playable.EntryChanged += (s, e) => EntryChanged?.Invoke(this, e);
+            _playable.EntryChanging += (s, e) => EntryChanging?.Invoke(this, e);
+        }
+
+        public T Entry => _playable.Entry;
+
+        public event EventHandler EntryChanged;
+        public event EntryChangingEventHandler<T> EntryChanging;
+
+        public virtual bool MoveNext() => _playable.MoveNext();
+    }
+}
---
+++ Source/Playlist/DecoratorPlaylist.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeadDog.Audio.Playlist
+{
+    public abstract class DecoratorPlaylist<T> : IPlaylist<T>
+    {
+        private readonly IPlaylist<T> _playlist;
+
+        public DecoratorPlaylist(IPlaylist<T> playlist)
+        {
+            _playlist = playlist ?? throw new ArgumentNullException(nameof(playlist));
+
+            _playlist.EntryChanged += (s, e) => EntryChanged?.Invoke(this, e);
+            _playlist.EntryChanging += (s, e) => EntryChanging?.Invoke(this, e);
+        }
+
+        public T Entry => _playlist.Entry;
+
+        public event EventHandler EntryChanged;
+        public event EntryChangingEventHandler<T> EntryChanging;
+
+        public virtual void Reset() => _playlist.Reset();
+
+        public virtual bool MoveNext() => _playlist.MoveNext();
+        public virtual bool MovePrevious() => _playlist.MovePrevious();
+
+        public virtual bool MoveToFirst() => _playlist.MoveToFirst();
+        public virtual bool MoveToLast() => _playlist.MoveToLast();
+
+        public virtual bool MoveToEntry(T entry) => _playlist.MoveToEntry(entry);
+        public virtual bool Contains(T entry) => _playlist.Contains(entry);
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return (_playlist as IEnumerable<T>).GetEnumerator();
+        }
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return (_playlist as System.Collections.IEnumerable).GetEnumerator();
+        }
+    }
+}
