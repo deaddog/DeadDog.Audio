424fb118c0bb507842c43631815a4273edb89496
deaddog <mikkelslsl@gmail.com>
2017-07-17 17:26:15 +0200

    Added optional custom sorting to the TrackCollectionPlaylist.

--- Source/Playlist/TrackCollectionPlaylist.cs
+++ Source/Playlist/TrackCollectionPlaylist.cs
@@ -6,21 +6,34 @@ namespace DeadDog.Audio.Playlist
 {
     public class TrackCollectionPlaylist : DecoratorPlaylist<Track>
     {
+        private readonly SortedPlaylist<Track> _sortedPlaylist;
         private readonly Playlist<Track> _playlist;
         private readonly LibraryCollection<Track> _collection;
 
-        public TrackCollectionPlaylist(LibraryCollection<Track> collection) : this(collection, new Playlist<Track>())
+        public TrackCollectionPlaylist(LibraryCollection<Track> collection, Comparison<Track> comparison = null)
+            : this(collection, comparison, comparison == null ? null : new SortedPlaylist<Track>(comparison), comparison == null ? new Playlist<Track>() : null)
         {
         }
-        private TrackCollectionPlaylist(LibraryCollection<Track> collection, Playlist<Track> playlist) : base(playlist)
+        private TrackCollectionPlaylist(LibraryCollection<Track> collection, Comparison<Track> comparison, SortedPlaylist<Track> sortedPlaylist, Playlist<Track> playlist) : base((IPlaylist<Track>)sortedPlaylist ?? playlist)
         {
+            _sortedPlaylist = sortedPlaylist;
             _playlist = playlist;
             _collection = collection;
 
-            foreach (var track in collection)
-                _playlist.Add(track);
+            if (_playlist != null)
+            {
+                foreach (var track in collection)
+                    _playlist.Add(track);
 
-            _collection.CollectionChanged += TracksCollectionChanged;
+                _collection.CollectionChanged += TracksCollectionChanged;
+            }
+            else if (_sortedPlaylist != null)
+            {
+                foreach (var track in collection)
+                    _sortedPlaylist.Add(track);
+
+                _collection.CollectionChanged += SortedTracksCollectionChanged;
+            }
         }
 
         private void TracksCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
@@ -42,6 +55,29 @@ namespace DeadDog.Audio.Playlist
                         throw new NotSupportedException("The playlist does not support more than one track moving at a time.");
                     else
                         _playlist.MoveEntry((Track)e.OldItems[0], e.NewStartingIndex);
+                    break;
+
+                default:
+                    throw new NotSupportedException($"The collection change action {e.Action} is not supported by the playlist.");
+            }
+        }
+
+        private void SortedTracksCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    for (int i = 0; i < e.NewItems.Count; i++)
+                        _sortedPlaylist.Add((Track)e.NewItems[i]);
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    for (int i = 0; i < e.OldItems.Count; i++)
+                        _playlist.Remove((Track)e.OldItems[i]);
+                    break;
+
+                case NotifyCollectionChangedAction.Move:
+                    // Move events are ignored for the sorted playlist
                     break;
 
                 default:
