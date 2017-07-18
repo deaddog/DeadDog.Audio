994ad79dff2fca84bfa1923a14b6c02647a57666
deaddog <mikkelslsl@gmail.com>
2017-07-18 07:37:31 +0200

    Defined an extension method for YouTubeID that combines the various steps in downloading youtube mp3 and writing id3 info to it.

---
+++ DeadDog.Audio.YouTube/Downloader.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace DeadDog.Audio.YouTube
+{
+    public static class Downloader
+    {
+        public static async Task<RawTrack> Download(this YouTubeID id, string destinationFilepath)
+        {
+            var meta = await YouTubeMediaParser.ParseYouTubeTitle(id, destinationFilepath);
+
+            await Executables.YoutubeDL.Download(id, destinationFilepath);
+            await Executables.FFmpeg.ApplyMetaData(meta);
+
+            return meta;
+        }
+    }
+}
