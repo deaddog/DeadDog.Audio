0612b2caba99668342011b8389b6b71e5dd5d3e2
deaddog <mikkelslsl@gmail.com>
2017-07-18 07:21:00 +0200

    Created wrappers for ffmpeg and youtube-dl.

---
+++ DeadDog.Audio.YouTube/Executables/YoutubeDL.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DeadDog.Audio.YouTube.Executables
+{
+    internal static class YoutubeDL
+    {
+        public static async Task Download(YouTubeID id, string destinationPath)
+        {
+            var executablePath = Paths.GetExecutablePath("youtube-dl.exe");
+
+            string downloadArgs = $"https://www.youtube.com/watch?v={id.ID} -x --audio-format mp3 --audio-quality 0 -o \"%(id)s.%(ext)s\"";
+            ProcessStartInfo downloadProc = new ProcessStartInfo(executablePath, downloadArgs)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+
+            await downloadProc.RunProcessAsync();
+
+            var filepath = Paths.GetExecutablePath($"{id.ID}.mp3");
+            File.Copy(filepath, destinationPath, true);
+            File.Delete(filepath);
+        }
+    }
+}
