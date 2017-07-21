100a6ccba0a49b037e2c2060c46aea02d08d9a70
deaddog <mikkelslsl@gmail.com>
2017-07-21 22:38:12 +0200

    Defined types that represent updates to a media scan.

---
+++ DeadDog.Audio/Scan/ScanProgress.cs
@@ -0,0 +1,27 @@
+﻿namespace DeadDog.Audio.Scan
+{
+    public struct ScanProgress
+    {
+        public ScannerStates State { get; }
+
+        public int Added { get; }
+        public int Updated { get; }
+        public int Skipped { get; }
+        public int Errors { get; }
+        public int Removed { get; }
+        public int Total { get; }
+
+        public ScanProgress(ScannerStates state, int added, int updated, int skipped, int errors, int removed, int total)
+        {
+            State = state;
+            Added = added;
+            Updated = updated;
+            Skipped = skipped;
+            Errors = errors;
+            Removed = removed;
+            Total = total;
+        }
+
+        public double Progress => (Added + Updated + Skipped + Errors + Removed) / (double)Total;
+    }
+}
---
+++ DeadDog.Audio/Scan/ScannedFile.cs
@@ -0,0 +1,19 @@
+﻿namespace DeadDog.Audio.Scan
+{
+    public struct ScannedFile
+    {
+        public ScannedFile(string filepath, RawTrack mediaInfo, FileActions action, ScanProgress scanProgress)
+        {
+            Filepath = filepath;
+            MediaInfo = mediaInfo;
+            Action = action;
+            ScanProgress = scanProgress;
+        }
+
+        public string Filepath { get; }
+        public RawTrack MediaInfo { get; }
+        public FileActions Action { get; }
+
+        public ScanProgress ScanProgress { get; }
+    }
+}
