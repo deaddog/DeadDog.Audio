7057fc855c7e48e110ffa364a5ab7279a17e35fb
deaddog <mikkelslsl@gmail.com>
2017-07-24 20:13:34 +0200

    Created a model specifically for album searching.

---
+++ DeadDog.Audio.AudioDB/Model/AudioDbAlbumSearchResult.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace DeadDog.Audio.AudioDB.Model
+{
+    public class AudioDbAlbumSearchResult
+    {
+        [JsonProperty(PropertyName = "album")]
+        public List<AudioDbAlbum> Albums { get; set; }
+    }
+}
