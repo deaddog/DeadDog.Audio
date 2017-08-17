c892f73e51a066f0004f7f7a4b5fee937dbc33d9
deaddog <mikkelslsl@gmail.com>
2017-08-17 22:05:17 +0200

    Redefined ID3v1/ID3v2 readers as implementations of IMediaParser.

--- DeadDog.Audio/Parsing/ID3/ID3Parser.cs
+++ DeadDog.Audio/Parsing/ID3/ID3Parser.cs
@@ -2,55 +2,21 @@
 {
     public class ID3Parser : IMediaParser
     {
+        private readonly ID3v1Parser _v1Parser;
+        private readonly ID3v2Parser _v2Parser;
+
+        public ID3Parser()
+        {
+            _v1Parser = new ID3v1Parser();
+            _v2Parser = new ID3v2Parser();
+        }
+
         public bool TryParseTrack(string filepath, out RawTrack track)
         {
-            var v1Info = ID3v1.FromFile(filepath);
-            var v2Info = new ID3v2(filepath);
-
-            if (!v1Info.TagFound && !v2Info.TagFound)
-            {
-                track = null;
-                return false;
-            }
-
-            var artist = Longest(v2Info.Artist, v1Info.Artist);
-            var album = Longest(v2Info.Album, v1Info.Album);
-            var title = Longest(v2Info.Title, v1Info.Title);
-
-            var tracknumber = GetTrackNumber(v1Info, v2Info);
-            var year = GetYear(v1Info, v2Info);
-
-            track = new RawTrack(filepath, title, album, tracknumber, artist, year);
-            return true;
-        }
-
-        private string Longest(string a, string b)
-        {
-            if (a == null)
-                return b;
-            if (b == null)
-                return a;
-
-            if (a.Length >= b.Length)
-                return a;
-            return b;
-        }
-
-        private int? GetTrackNumber(ID3v1 v1, ID3v2 v2)
-        {
-            if (v2.TrackNumber > 0)
-                return v2.TrackNumber;
-            else if (v1.TrackNumber > 0)
-                return v1.TrackNumber;
+            if (_v2Parser.TryParseTrack(filepath, out track))
+                return true;
             else
-                return null;
-        }
-        private int? GetYear(ID3v1 v1, ID3v2 v2)
-        {
-            if (v2.Year > 0)
-                return v2.Year;
-            else
-                return v1.Year;
+                return _v1Parser.TryParseTrack(filepath, out track);
         }
     }
 }
---
+++ DeadDog.Audio/Parsing/ID3/v2/ID3v2Parser.cs
@@ -0,0 +1,85 @@
+﻿using System.IO;
+using System.Text;
+
+namespace DeadDog.Audio.Parsing.ID3
+{
+    internal class ID3v2Parser : IMediaParser
+    {
+        public bool TryParseTrack(string filepath, out RawTrack track)
+        {
+            using (var fs = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+            {
+                FrameReader reader;
+                try
+                {
+                    reader = new FrameReader(fs);
+                    track = GetTrack(filepath, reader);
+                }
+                catch
+                {
+                    track = null;
+                    return false;
+                }
+
+                if (TagHeader.IsEmpty(reader.TagHeader) || reader.FrameCount == 0)
+                {
+                    track = null;
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private RawTrack GetTrack(string filepath, FrameReader reader)
+        {
+            var title = reader.ReadString("TIT2");
+            var artist = reader.ReadString("TPE1");
+            var album = reader.ReadString("TALB");
+            var year = GetYear(reader);
+            var tracknumber = GetTracknumber(reader);
+
+            return new RawTrack(filepath, title, album, tracknumber, artist, year);
+        }
+
+        private int? GetYear(FrameReader reader)
+        {
+            var yearstring = reader.ReadString("TYER");
+            if (yearstring == null)
+                return null;
+            else if (int.TryParse(yearstring, out int year))
+                return year;
+            else
+                return null;
+        }
+        private int? GetTracknumber(FrameReader reader)
+        {
+            var trackstring = reader.ReadString("TRCK");
+
+            if (trackstring == null)
+                return null;
+            else if (trackstring.Contains("/"))
+            {
+                string s = trackstring.Substring(0, trackstring.IndexOf('/'));
+                if (int.TryParse(s, out var tracknumber))
+                    return tracknumber;
+                else
+                    return null;
+            }
+            else
+            {
+                var sb = new StringBuilder();
+                for (int i = 0; i < trackstring.Length; i++)
+                {
+                    if (char.IsDigit(trackstring[i]))
+                        sb.Append(trackstring[i]);
+                }
+
+                if (int.TryParse(sb.ToString(), out var tracknumber))
+                    return tracknumber;
+                else
+                    return null;
+            }
+        }
+    }
+}
